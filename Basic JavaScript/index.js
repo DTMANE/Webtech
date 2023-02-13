9896c5d16fe1dbe357c3db377b544272b86e28a4
Mane <dinesh.tanaji-mane@capgemini.com>
2023-02-13 09:41:41 +0530

    css1

---
+++ Basic JavaScript/index.js
@@ -0,0 +1,13 @@
+var a;
+var b;
+var c = a + b;
+
+function add() {
+  alert("Sum of " + a + " and " + b + "=" + c);
+}
+function value() {
+  var a = prompt("Enter value of a ");
+  var b = prompt("Enter value of b ");
+}
+value();
+add();
